6c258c356b6718600112c9954fb7f1a484389938
Lynch <58525501+alynch-code@users.noreply.github.com>
2020-05-31 17:22:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var img1 = document.querySelectorAll("img")[0];
+var img2 = document.querySelectorAll("img")[1];
+
+var diceImage1 = "images/dice"+randomNumber1+".png";
+var diceImage2 = "images/dice"+randomNumber2+".png";
+
+var title = document.querySelector("h1");
+
+img1.setAttribute("src", diceImage1);
+img2.setAttribute("src", diceImage2);
+
+if(randomNumber2<randomNumber1){
+  title.textContent = "Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber1){
+  title.textContent = "Player 2 Wins!";
+}
+else{
+  title.textContent = "Draw!";
+}
